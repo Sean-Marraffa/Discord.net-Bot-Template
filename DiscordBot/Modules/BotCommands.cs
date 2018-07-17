ec780c54181cb15ea139b348a4b7031d6ab5fa24
Sean Marraffa <Sean@photofied.com>
2018-07-16 20:19:42 -0400

    Add project files.

---
+++ DiscordBot/Modules/BotCommands.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    [Group("DiscordBot")]
+    public class BotCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello")]
+        public async Task HelloCommand()
+        {
+            await ReplyAsync($"Hello, {Context.User.Mention}!");
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            // Replace My Token With Your Discord Bot Token
+            string botToken = "NDY4MjY1Mjg5ODY0NDQ1OTY0.Di7CYQ.XewkBLLdMJeS_zngtOT_40fEp_4";
+
+            // Event Logging
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            // If Message Is NULL Or Message Is Sent By Bot, Ignore Message
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            // If Message Has A ! Prefix Or The Bot Is Mentioned, Handle The Command
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
